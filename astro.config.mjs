7480f36267e5b6c7368a1188f0941e7a22f7af06
Chukwuemeka Aligbe <cealigbe@gmail.com>
2026-07-18 21:24:40 -0400

    added RSS and Greeting component

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@
 import { defineConfig, fontProviders } from 'astro/config';
 
 import mdx from '@astrojs/mdx';
+import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://chukwali.netlify.app",
   fonts: [{
       provider: fontProviders.fontsource(),
       name: 'Geist',
@@ -15,5 +17,5 @@ export default defineConfig({
       cssVariable: '--font-geist-mono',
   }],
 
-  integrations: [mdx()],
+  integrations: [mdx(), svelte()],
 });
---
+++ src/components/Greeting.svelte
@@ -0,0 +1,35 @@
+<script>
+    let { messages } = $props();
+
+    let greeting = $state("Hello");
+
+    function randomMessage() {
+      let message = messages[Math.floor(Math.random() * messages.length)];
+      greeting = message;
+      // console.log(message);
+    }
+</script>
+
+<div class="greeting">
+    <h3><span class="notice">{greeting}!</span> Thank you for visiting.</h3>
+
+    <button onclick={randomMessage}>
+        New Greeting
+    </button>
+</div>
+
+<style>
+    .greeting {
+        width: 100%;
+        margin-bottom: 2rem;
+        padding: 2rem;
+        border: 2px solid var(--clr-primary-light);
+        border-radius: 1rem;
+        border-bottom-right-radius: 0;
+    }
+
+    .notice {
+        text-decoration: underline;
+        color: var(--clr-primary-dark);
+    }
+</style>
